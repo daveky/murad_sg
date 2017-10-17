0e8308ad2b923fcaa028024f7e541b511b3119bc
Unknown <daveky@gmail.com>
2017-10-16 23:36:54 -0700

    Revert "Revert "uploading files""
    This reverts commit 4757da08d5c22fb76aca35b553fa33ef33a1f30e.

---
+++ src/App.vue
@@ -0,0 +1,95 @@
+<template>
+
+  <v-app
+    id="inspire"
+  >
+    <v-navigation-drawer
+      persistent
+      clipped
+      enable-resize-watcher
+      v-model="drawer"
+      app
+      dark
+    >
+      <v-list>
+        <v-subheader class="mt-3 text--darken-1">SUBSCRIPTIONS</v-subheader>
+
+        <v-list>
+          <v-list-group v-for="item in items" :value="item.active" v-bind:key="item.title">
+            <v-list-tile slot="item" ripple @click="">
+              <v-list-tile-action>
+                <v-icon>{{ item.action }}</v-icon>
+              </v-list-tile-action>
+              <v-list-tile-content>
+                <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+              </v-list-tile-content>
+              <v-list-tile-action>
+                <v-icon>keyboard_arrow_down</v-icon>
+              </v-list-tile-action>
+            </v-list-tile>
+            <v-list-tile v-for="subItem in item.items" v-bind:key="subItem.title" v-bind:to="subItem.to" ripple @click="">
+              <v-list-tile-content>
+                <v-list-tile-title>{{ subItem.title }}</v-list-tile-title>
+              </v-list-tile-content>
+              <v-list-tile-action>
+                <v-icon>{{ subItem.action }}</v-icon>
+              </v-list-tile-action>
+            </v-list-tile>
+          </v-list-group>
+        </v-list>
+
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar color="transparent" flat dense fixed clipped-left app>
+      <v-toolbar-title style="width: 300px" class="ml-0 pl-3">
+        <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+      </v-toolbar-title>
+      <v-toolbar-title class="">{{ pageTitle }}</v-toolbar-title>
+    </v-toolbar>
+    <main>
+      <v-content>
+
+        <router-view />
+
+      </v-content>
+    </main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data: () => ({
+    pageTitle: '[pageTitle]',
+    drawer: true,
+    items: [
+        {
+          action: 'school',
+          title: 'Components',
+          items: [
+            { title: 'Header', to: '/header' },
+            { title: 'Buttons', to: '/buttons' },
+            { title: 'Breadcrumbs' },
+            { title: 'Forms' },
+            { title: 'Lits' },
+            { title: 'Tabs' }
+          ]
+        },
+      ]
+  }),
+  props: {
+    source: String
+  }
+}
+</script>
+
+<style>
+.fadeIn {
+  animation: fadein .5s;
+}
+
+@keyframes fadeIn {
+    from { opacity: 0; }
+    to   { opacity: 1; }
+}
+</style>
---
+++ src/components/Buttons.vue
@@ -0,0 +1,71 @@
+<template>
+  <div id="buttons" class="fadeIn">
+    <v-container fluid>
+      <h1>
+        Buttons
+      </h1>
+      <button class="button">Buy Now</button>
+
+      <div class="product-essential">
+        <div class="product-right-contents">
+          <div class="add-to-cart-wrapper">
+            <div class="add-to-box">
+              <div class="add-to-cart">
+                <div class="add-to-cart-buttons">
+                  <button id="disable-btn" type="button" title="Add to Bag" class="button btn-cart clubkit clubkit-disabled product-1264  sku-15250 clubkit" data-controlleraction="add" data-product_id="1264" onclick="productAddToCartForm.submit(this)"><span><span>Add to Bag</span></span>
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="main-block">
+        <ul class="three-block">
+          <li>
+            <div>
+              <div>
+                <p class="underline"><a href="/product/rapid-lightening-30-day-kit/">Shop Now</a></p>
+              </div>
+            </div>
+          </li>
+        </ul>
+      </div>
+
+      <div id="subcatlist">
+        <ul class="products-grid products-grid--max-4-col four-column-product-grid" id="catlist">
+          <li class="item gateway-container">
+            <a href="https://www.murad.com/anti-aging/menopause-treatments" class="scroll">
+              <div class="gateway-img">
+                <!--           images should be 300x390-->
+                <img src="https://6b901r-8ieybj5zerw6.webscalenetworks.net/HJzz2xIdH/media/catalog/category/a60361.1505517973.jpg.pagespeed.ic.OfFUsqCIPb.webp" alt="" data-pagespeed-url-hash="2610495335" onload="pagespeed.CriticalImages.checkImageForCriticality(this);">
+              </div>
+              <div class="gateway-label">
+                <span>Loss of Firmness &amp; Deep Wrinkles</span>
+              </div>
+            </a>
+          </li>
+        </ul>
+      </div>
+
+
+
+
+    </v-container>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'buttons',
+  pageTitle: 'Buttons',
+  data () {
+    return {
+    }
+  },
+  created: function() {
+      this.$parent.$emit('setPageTitle', this.pageTitle)
+  }
+}
+</script>
