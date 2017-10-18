697daad3a5acc888276aca781c358512577812f2
Dave Ky <daveky@gmail.com>
2017-10-18 01:03:55 -0700

    fixed global scss

--- src/App.vue
+++ src/App.vue
@@ -87,27 +87,4 @@ export default {
 }
 </script>
 
-<style src="./assets/scss/global.scss" lang="scss">
-.fadeIn {
-  animation: fadein .5s;
-}
-
-@keyframes fadeIn {
-    from { opacity: 0; }
-    to   { opacity: 1; }
-}
-
-/* Enter and leave animations can use different */
-/* durations and timing functions.              */
-.slide-fade-enter-active {
-  transition: all .3s ease;
-}
-.slide-fade-leave-active {
-  transition: all .8s cubic-bezier(1.0, 0.5, 0.8, 1.0);
-}
-.slide-fade-enter, .slide-fade-leave-to
-/* .slide-fade-leave-active below version 2.1.8 */ {
-  transform: translateX(10px);
-  opacity: 0;
-}
-</style>
+<style src="./assets/scss/global.scss" lang="scss"></style>
